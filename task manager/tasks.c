c836606dd5771db70d4511ce7a76eae3f51bcbe9
onepercentishuman <96989030+Aruna1111@users.noreply.github.com>
2023-02-25 02:58:44 +0600

    Create tasks.c

---
+++ task manager/tasks.c
@@ -0,0 +1,42 @@
+#include <linux/delay.h> 
+#include <linux/interrupt.h> 
+#include <linux/kernel.h> 
+#include <linux/module.h> 
+ 
+/* Макрос DECLARE_TASKLET_OLD присутствует для совместимости. 
+ * См. https://lwn.net/Articles/830964/.
+ */ 
+#ifndef DECLARE_TASKLET_OLD 
+#define DECLARE_TASKLET_OLD(arg1, arg2) DECLARE_TASKLET(arg1, arg2, 0L) 
+#endif 
+ 
+static void tasklet_fn(unsigned long data) 
+{ 
+    pr_info("Example tasklet starts\n"); 
+    mdelay(5000); 
+    pr_info("Example tasklet ends\n"); 
+} 
+ 
+static DECLARE_TASKLET_OLD(mytask, tasklet_fn); 
+ 
+static int example_tasklet_init(void) 
+{ 
+    pr_info("tasklet example init\n"); 
+    tasklet_schedule(&mytask); 
+    mdelay(200); 
+    pr_info("Example tasklet init continues...\n"); 
+    return 0; 
+} 
+ 
+static void example_tasklet_exit(void) 
+{ 
+    pr_info("tasklet example exit\n"); 
+    tasklet_kill(&mytask); 
+} 
+ 
+module_init(example_tasklet_init); 
+module_exit(example_tasklet_exit); 
+ 
+MODULE_DESCRIPTION("Tasklet example"); 
+MODULE_LICENSE("GPL");
+
